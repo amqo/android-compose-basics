a888e0f79fd60482b9a6ead30c0265147989af32
Alberto Muñoz <alberto.mqo@gmail.com>
2022-07-09 14:44:57 +0200

    Change expand button

--- app/src/main/java/com/amqo/compose_basics/MainActivity.kt
+++ app/src/main/java/com/amqo/compose_basics/MainActivity.kt
@@ -11,8 +11,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -59,14 +65,6 @@ fun Greeting(name: String) {
 
     var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
     Card(
         backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -75,9 +73,9 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .padding(12.dp)
             ) {
-                GreetingColumnContent(name = name)
+                GreetingColumnContent(expanded = expanded, name = name)
             }
             ExpandButton(onChangeExpand = { expanded = !expanded }, expanded = expanded)
         }
@@ -85,21 +83,29 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun GreetingColumnContent(name: String) {
+fun GreetingColumnContent(expanded: Boolean, name: String) {
     Text(text = stringResource(R.string.hello))
     Text(text = name, style = MaterialTheme.typography.h4.copy(
         fontWeight = FontWeight.ExtraBold
     ))
+    if (expanded) {
+        Text(
+            text = ("Composem ipsum color sit lazy, " +
+                    "padding theme elit, sed do bouncy. ").repeat(4)
+        )
+    }
 }
 
 @Composable
 fun ExpandButton(onChangeExpand: () -> Unit, expanded: Boolean) {
-    OutlinedButton(
-        onClick = onChangeExpand
-    ) {
-        Text(
-            if (expanded) stringResource(R.string.show_less)
-            else stringResource(R.string.show_more)
+    IconButton(onClick = onChangeExpand) {
+        Icon(
+            imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+            contentDescription = if (expanded) {
+                stringResource(R.string.show_less)
+            } else {
+                stringResource(R.string.show_more)
+            }
         )
     }
 }
